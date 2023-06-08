080a60388f583674d87ffe13383a214709a02f8b
Ramy <ramy.chamseddin-el-saghir@capgemini.com>
2023-06-08 17:26:06 +0200

    created first draft for update booking

---
+++ APIs/TRM_UpdateBooking.py
@@ -0,0 +1,95 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    # Receive the Post Data from the API
+    meetingroomID = event["meetingroomID"]
+    date = event["date"]
+    start_time = event["start_time"]
+    end_time = event["end_time"]
+    booking_code = event["booking_code"]
+    employeeID = event["employeeID"]
+
+    # Receive the email Attributes from the API
+    to_address = event["to_address"]
+    subject = event['subject']
+    body = event['body']
+    from_address = event['from_address']
+
+    body_message = f"""{body}
+    Your booking has been updated as below:
+    \tMeetingroom: {meetingroomID}
+    \tDate: {date}
+    \tStart time: {start_time}
+    \tEnd time: {end_time}
+    \tBooking code: {booking_code}
+
+    To edit or cancel this booking, please click the following link: https://example.com
+
+    Kind regards
+    Your booking team"""
+
+    # load the DynamoDB client
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("TRM_MeetingRoom_Booking")
+
+    # load the ses client
+    client = boto3.client('ses')
+
+    #if get:
+        # TODO: write code...
+
+    try:
+        post_response = table.put_item(
+            Item={
+                "meetingroomID": meetingroomID,
+                "date": date,
+                "start_time": start_time,
+                "end_time": end_time,
+                "booking_code": booking_code,
+                "employeeID": employeeID
+            }
+        )
+
+        if post_response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            email_response = client.send_email(
+                Source=from_address,
+                Destination={
+                    'ToAddresses':
+                        [
+                            to_address
+                        ]
+
+                },
+                Message={
+                    'Subject': {
+                        'Data': subject
+                    },
+                    'Body': {
+                        'Text': {
+                            'Data': body_message
+
+                        }
+                    }
+
+                }
+            )
+
+            return {
+                "status": 200,
+                "body": json.dumps("data posted successfully"),
+                'body': json.dumps(email_response)
+            }
+
+        else:
+            return {
+                "status": 400,
+                "body": json.dumps("data failed to post")
+            }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
