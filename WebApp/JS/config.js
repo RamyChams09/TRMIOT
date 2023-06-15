bda8415455a1467ad1f6180477cbfaf241885d25
TanjaKutzner <tanja.kutzner@capgemini.com>
2023-06-15 20:42:24 +0200

    registration, verification and login and also a bit of styling, not pretty, but it works

---
+++ WebApp/JS/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_lpgDFIdQ1',
+        userPoolClientId: '709eu8fla6cpc05b71hgebg8f5',
+        region: 'eu-central-1'
+    },
+    api: {
+        invokeUrl: 'https://tgjdqpmdj0.execute-api.eu-central-1.amazonaws.com/Dev'
+    }
+};
