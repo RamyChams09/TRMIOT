10cf7ae3709dd54efe0847fb2097bac9e725eb7d
JessicaD78 <98092221+kwameZION@users.noreply.github.com>
2023-06-26 16:53:05 +0200

    GET Function has been changed

--- WebApp/JS/roombooking.js
+++ WebApp/JS/roombooking.js
@@ -87,7 +87,75 @@ function bookRoom() {
         console.error('Error:', error);
       });
 
+    });
+
+  }
+
+
+
     // Get Function
+
+    function OnloadBooking() {
+      var apiUrl = 'https://tgjdqpmdj0.execute-api.eu-central-1.amazonaws.com/Dev/';
+      
+      var roomID = document.getElementById('roomSelect').value;
+      var booking_date = document.getElementById('date').value;
+      var start_time = document.getElementById('start_time').value;
+      var end_time = document.getElementById('end_time').value;
+    
+      // Validate if the selected date and time are in the future
+      var currentTime = new Date().getTime();
+      var selectedDateTime = new Date(`${booking_date}T${start_time}`).getTime();
+    
+      if (selectedDateTime < currentTime) {
+        alert('Please select a future date and time.');
+        return;
+      }
+    
+      var postData = {
+        'meetingroomID': roomID,
+        'booking_date': booking_date,
+        'start_time': start_time,
+        'end_time': end_time,
+        // 'employeeID': 'Jessica',
+      };
+    
+      console.log(postData);
+    
+      var poolData = {
+        UserPoolId: 'eu-central-1_lpgDFIdQ1', // Replace with your Cognito User Pool ID
+        ClientId: '709eu8fla6cpc05b71hgebg8f5', // Replace with your Cognito App Client ID
+      };
+    
+      var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+      console.log(`our user pool: ${userPool}`)
+    
+      if (typeof AWSCognito !== 'undefined') {
+        AWSCognito.config.region = 'eu-central-1';
+      }
+    
+      var authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
+    
+        if (cognitoUser) {
+          cognitoUser.getSession(function sessionCallback(err, session) {
+            if (err) {
+              reject(err);
+            } else if (!session.isValid()) {
+              resolve(null);
+            } else {
+              resolve(session.getIdToken().getJwtToken());
+            }
+          });
+        } else {
+          console.log("we have no user")
+          resolve(null);
+        }
+      });
+    
+      authToken.then(function setAuthToken(token) {
+        console.log(token);
+
     fetch(apiUrl, {
       method: 'GET',
       headers: {
@@ -115,6 +183,8 @@ function bookRoom() {
       });
 
     });
+
+    
 
   }
 
@@ -196,8 +266,6 @@ function bookRoom() {
       });
 
     }
+  
 
     */
-
-        
-
