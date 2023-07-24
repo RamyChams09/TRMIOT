0f533a4e94678f9376c630625bc73134ecf12da4
TanjaKutzner <tanja.kutzner@capgemini.com>
2023-07-24 10:29:08 +0200

    cleaned and shortened the code

--- WebApp/JS/dateDisplayOccupancy.js
+++ WebApp/JS/dateDisplayOccupancy.js
@@ -1,232 +1,185 @@
-// Get the necessary HTML elements
-var previousBtn = document.getElementById('previousBtn');
-var nextBtn = document.getElementById('nextBtn');
-var dateDisplay = document.getElementById('dateDisplay');
+/*global TRMIOT _config*/
 
-// Initialize the current date
-var currentDate = new Date();
+var TRM_RoomBooking_API = window.TRM_RoomBooking_API || {};
 
-// Display the current date and day
-displayDate();
+(function dateDisplayOccupancyScopeWrapper($) {
 
-// Event listener for the Next button
-nextBtn.addEventListener('click', function() {
-  // Update the currentDate for the next day
-  currentDate.setDate(currentDate.getDate() + 1);
-  displayDate();
-});
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
 
-// Event listener for the Previous button
-previousBtn.addEventListener('click', function() {
-  // Update the currentDate for the previous day
-  currentDate.setDate(currentDate.getDate() - 1);
-  displayDate();
-});
-
-// Function to display the current date and day
-function displayDate() {
-  // Format the date as DD-MM-YYYY
-  var formattedDate = formatDate(currentDate);
-  dateDisplay.textContent = formattedDate;
-
-  // Get the day of the week
-  var dayOfWeek = getDayOfWeek(currentDate);
-  dateDisplay.textContent += ' (' + dayOfWeek + ')';
-
-  // Enable the Next button
-  nextBtn.disabled = false;
-
-  // Check if it is the current date and disable the Next button
-  if (isCurrentDate(currentDate)) {
-    nextBtn.disabled = true;
-  }
-
-  // Call the getFunction()
-  getOccupancy(formattedDate);
-}
-
-// Function to check if a date is the current date
-function isCurrentDate(date) {
-  var today = new Date();
-  return date.toDateString() === today.toDateString();
-}
-
-// Function to format a date as DD-MM-YYYY
-function formatDate(date) {
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  var year = date.getFullYear();
-
-  // Add leading zeros if necessary
-  day = day < 10 ? '0' + day : day;
-  month = month < 10 ? '0' + month : month;
-
-  return year + '-' + month + '-' + day;
-}
-
-// Function to get the day of the week
-function getDayOfWeek(date) {
-  var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  var dayIndex = date.getDay();
-  return daysOfWeek[dayIndex];
-}
-
-
-function plotGraph(responseData, room_id){
-  // Sample data for plotting
-  var hours = Array.from(Array(24).keys()).map(i => ("0" + (i + 1)).slice(-2) + ":00");
-
-  // Create the chart context
-  var ctx = document.getElementById('occupancyChart' + room_id).getContext('2d');
-
-   // Set the line color based on room_id
-  var lineColor;
-    if (room_id === '1') {
-      lineColor = '#3498DB'; // Blue for room 1
-    } else if (room_id === '2') {
-      lineColor = '#55B4B0'; // Green for room 2
+    if (!(_config.cognito.userPoolId &&
+        _config.cognito.userPoolClientId &&
+        _config.cognito.region)) {
+            $('#noCognitoMessage').show();
+        return;
     }
 
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-  if (room_id === '1') {
-    let chartStatus = Chart.getChart("occupancyChart1");
-    if (chartStatus != undefined) {
-      chartStatus.destroy();
+    if (typeof AWSCognito !== 'undefined') {
+        AWSCognito.config.region = _config.cognito.region;
     }
 
-    let chartStatus1 = Chart.getChart("occupancyChart2");
-    if (chartStatus1 != undefined) {
-      chartStatus1.destroy();
-    }
-  }
+    TRM_RoomBooking_API.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
   
-  // Create the occupancy chart
-  var occupancyChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: responseData['time'],
-      datasets: [{
-        label: 'Room' + room_id,
-        data: responseData['occupancy'],
-        borderColor: lineColor, // Set the line color
-        borderWidth: 5, // Set the line width
-        fill: false
-      }],
-      
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-          max: 10,
-          title: {
-            display: true,
-            text: 'Occupancy'
-          }
-        },
-        x: {
-          beginAtZero: true,
-          max: 1440,
-          title: {
-            display: true,
-            text: 'Time'
-          }
+        if (cognitoUser) {
+            cognitoUser.getSession(function sessionCallback(err, session) {
+                if (err) {
+                    reject(err);
+                } else if (!session.isValid()) {
+                    resolve(null);
+                } else {
+                    resolve(session.getIdToken().getJwtToken());
+                }
+            });
+        } else {
+            console.log("The user does not exist or is not logged in.")
+            resolve(null);
         }
-      },
-      responsive: true,
-      maintainAspectRatio: true
+    });
+
+    var previousBtn = document.getElementById('previousBtn');
+    var nextBtn = document.getElementById('nextBtn');
+    var dateDisplay = document.getElementById('dateDisplay');
+
+    var currentDate = new Date();
+
+    displayDate();
+
+    nextBtn.addEventListener('click', function() {
+        currentDate.setDate(currentDate.getDate() + 1);
+        displayDate();
+    });
+
+    previousBtn.addEventListener('click', function() {
+        currentDate.setDate(currentDate.getDate() - 1);
+        displayDate();
+    });
+
+    function displayDate() {
+        var formattedDate = formatDate(currentDate);
+        var dayOfWeek = getDayOfWeek(currentDate);
+
+        dateDisplay.textContent = formattedDate + ' (' + dayOfWeek + ')';
+        nextBtn.disabled = isCurrentDate(currentDate);
+        getOccupancy(formattedDate);
     }
-  });
-  //occupancyChart.destroy();
-}
 
-function getOccupancy(formattedDate) {
+    function isCurrentDate(date) {
+        var today = new Date();
+        return date.toDateString() === today.toDateString();
+    }
 
-var poolData = {
-  UserPoolId: 'eu-central-1_lpgDFIdQ1', // Replace with your Cognito User Pool ID
-  ClientId: '709eu8fla6cpc05b71hgebg8f5', // Replace with your Cognito App Client ID
-};
+    function formatDate(date) {
+        var day = date.getDate().toString().padStart(2, '0');
+        var month = (date.getMonth() + 1).toString().padStart(2, '0');
+        var year = date.getFullYear();
 
-var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+        return year + '-' + month + '-' + day;
+    }
 
-if (typeof AWSCognito !== 'undefined') {
-  AWSCognito.config.region = 'eu-central-1';
-}
+    function getDayOfWeek(date) {
+        var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var dayIndex = date.getDay();
+        return daysOfWeek[dayIndex];
+    }
 
-var authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
-    var cognitoUser = userPool.getCurrentUser();
+    async function plotGraph(responseData, room_id){
 
-    if (cognitoUser) {
-        cognitoUser.getSession(function sessionCallback(err, session) {
-            if (err) {
-                reject(err);
-            } else if (!session.isValid()) {
-                resolve(null);
-            } else {
-                resolve(session.getIdToken().getJwtToken());
+        // Create the chart context
+        var ctx = document.getElementById('occupancyChart' + room_id).getContext('2d');
+
+        var lineColor;
+        if (room_id === '1') {
+            lineColor = '#3498DB';
+        } else if (room_id === '2') {
+            lineColor = '#55B4B0';
+        }
+
+        if (room_id === '1') {
+            let chartStatus = Chart.getChart("occupancyChart1");
+            if (chartStatus != undefined) {
+                chartStatus.destroy();
+            }
+
+            let chartStatus1 = Chart.getChart("occupancyChart2");
+            if (chartStatus1 != undefined) {
+                chartStatus1.destroy();
+            }
+        }
+    
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: responseData['time'],
+                datasets: [{
+                    label: 'Room' + room_id,
+                    data: responseData['occupancy'],
+                    borderColor: lineColor,
+                    borderWidth: 5,
+                    fill: false
+                }],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        max: 10,
+                        title: {
+                            display: true,
+                            text: 'Occupancy'
+                        }
+                    },
+                    x: {
+                        beginAtZero: true,
+                        max: 1440,
+                        title: {
+                            display: true,
+                            text: 'Time'
+                        }
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: true
             }
         });
-    } else {
-        console.log("we have no user")
-        resolve(null);
     }
-});
 
-authToken.then(function setAuthToken(token) {
-
-
-  var apiUrl = 'https://tgjdqpmdj0.execute-api.eu-central-1.amazonaws.com/Dev/getoccupancy?room_id=room_1&date=' + formattedDate;
-
-  fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': token,
-    },
-  })
-    .then(function(data) {
-      data.json()
-      .then(function (responseData) {
-        plotGraph(responseData, '1');
-        if (!data.ok) {
-          alert(responseData);
-      }
-
-      })
-      .catch(function (e) {
-        console.log(e);
-      });
-    })
-    .catch(function(error) {
-      console.error('Error:', error);
-    });
-
-
-var apiUrl = 'https://tgjdqpmdj0.execute-api.eu-central-1.amazonaws.com/Dev/getoccupancy?room_id=room_2&date=' + formattedDate;
-
-  fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': token,
-    },
-  })
-    .then(function(data) {
-      data.json()
-      .then(function (responseData) {
-        plotGraph(responseData, '2');
-        if (!data.ok) {
-          alert(responseData);
-      }
-
-      })
-      .catch(function (e) {
-        console.log(e);
-      });
-    })
-    .catch(function(error) {
-      console.error('Error:', error);
-    });
-  });
-
-}
+    async function getOccupancy(formattedDate) {
+        try {
+            var token = await TRM_RoomBooking_API.authToken;
+            var [data1, data2] = await Promise.all([
+                fetch(_config.api.invokeUrl + '/getoccupancy?room_id=room_1&date=' + formattedDate, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': token
+                    },
+                }),
+                fetch(_config.api.invokeUrl + '/getoccupancy?room_id=room_2&date=' + formattedDate, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': token
+                    },
+              }),
+            ]);
+      
+            var [responseData1, responseData2] = await Promise.all([data1.json(), data2.json()]);
+            plotGraph(responseData1, '1');
+            plotGraph(responseData2, '2');
+      
+            if (!data1.ok) {
+                alert(responseData1);
+            }
+            if (!data2.ok) {
+                alert(responseData2);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+}(jQuery));
